1a03bfd7882f695a3acf18e8e3ea2376044a8287
olzak33 <zawal.aleksander@gmail.com>
2024-09-13 23:46:12 +0200

    zadanie 3 praktyki
    program tworzy 100 fikcyjnych użytkowników i przyznaje im losowo: Imię, Nazwisko, rok urodzenia i liczbę porządkową.

---
+++ excel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class program
+{
+    static void Main()
+    {
+        string date = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+        string filename = $"users-{date}.txt";
+
+        string[] names = { "Ania", "Kasia", "Basia", "Zosia" };
+        string[] surnames = { "Kowalska", "Nowak" };
+        Random random = new Random();
+
+        StringBuilder csvfile = new StringBuilder();
+        csvfile.AppendLine("LP,Imie,Nazwisko,Rok urodzenia");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            string name = names[random.Next(names.Length)];
+            string surname = surnames[random.Next(surnames.Length)];
+            int birthday = random.Next(1990, 2001);
+            
+            csvfile.AppendLine($"{i} {name} {surname} {birthday}");
+        }
+
+        File.WriteAllText(filename, csvfile.ToString() );
+        Console.WriteLine($"file {filename} został utworzony");
+    }
+}
